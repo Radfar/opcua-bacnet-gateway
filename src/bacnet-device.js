aa0e1903d825c42a90478e85060f33d0764e2128
Alireza Radfar <atoagroup@gmail.com>
2026-09-25 21:01:41 +0330

    Implement Device Instance 10003 and respond to Who-Is with I-Am.

--- src/bacnet-device.js
+++ src/bacnet-device.js
@@ -1,16 +1,33 @@
 const Bacnet = require("node-bacnet");
 
+const DEVICE_INSTANCE = 10003;
+const DEVICE_VENDOR_ID = 999;
+const MAX_APDU = 1476;
+
 const options = {
-    port: 47808, // BACnet/IP default UDP port
+    port: 47808,
     interface: "0.0.0.0"
 };
 
 const client = new Bacnet(options);
 
 console.log("BACnet/IP device started");
+console.log(`Device Instance: ${DEVICE_INSTANCE}`);
 console.log("Listening on UDP port 47808");
 
-// Respond to Who-Is requests
+// Handle BACnet Who-Is requests
 client.on("whoIs", (device) => {
     console.log("Who-Is received:", device);
+
+    // Respond with I-Am
+    client.iAmResponse(
+        device.header.sender,
+        DEVICE_INSTANCE,
+        0,
+        DEVICE_VENDOR_ID
+    );
+
+    console.log(
+        `I-Am sent: Device ${DEVICE_INSTANCE} -> ${device.header.sender.address}`
+    );
 });
